f98d2f2e12910ff06bf1f6b1b69ac04542990c6b
N6REJ <programming@hallhome.us>
2021-11-07 14:37:30 -0600

    formating

--- database.lua
+++ database.lua
@@ -17,24 +17,24 @@ db.Filters = {
         uiDesc = "Reagents for Leatherworking in Shadowlands",
         title = "Leather",
         items = {
-            [172089] = true,    -- Desolate Leather
-            [172092] = true,    -- Pallid Bone
-            [172094] = true,    -- Callous Hide
-            [172096] = true,    -- Heavy Desolate Leather
-            [172097] = true,    -- Heavy Callous Hide
-            [172438] = true,    -- Enchanted Heavy Callous Hide
-            [177279] = true,    -- Gaunt Sinew
-        },
+            [172089] = true, -- Desolate Leather
+            [172092] = true, -- Pallid Bone
+            [172094] = true, -- Callous Hide
+            [172096] = true, -- Heavy Desolate Leather
+            [172097] = true, -- Heavy Callous Hide
+            [172438] = true, -- Enchanted Heavy Callous Hide
+            [177279] = true -- Gaunt Sinew
+        }
     },
     ["Cloth"] = {
         uiName = "Shadowlands Tailoring",
         uiDesc = "Reagents for tailoring in Shadowlands",
         title = "Cloth",
         items = {
-            [172439] = true,	-- Enchanted Lightless Silk
-            [173202] = true,	-- Shrouded Cloth
-            [173204] = true,	-- Lightless Silk
-        },
+            [172439] = true, -- Enchanted Lightless Silk
+            [173202] = true, -- Shrouded Cloth
+            [173204] = true -- Lightless Silk
+        }
     },
     ["Vendor_Reagents"] = {
         uiName = "Shadowlands Vendor Reagents",
@@ -42,11 +42,11 @@ db.Filters = {
         title = "Vendor Reagents",
         items = {
             -- ID    = true,    --Item name
-            [177062] = true,    -- Penumbra Thread
-            [178787] = true,    -- Orboreal shards
-            [177061] = true,    -- Twilight Bark
-            [183955] = true,    -- Curing Salt
-        },
+            [177062] = true, -- Penumbra Thread
+            [178787] = true, -- Orboreal shards
+            [177061] = true, -- Twilight Bark
+            [183955] = true -- Curing Salt
+        }
     },
     ["Crafted_Reagents"] = {
         uiName = "Shadowlands crafted reagents",
@@ -54,13 +54,13 @@ db.Filters = {
         title = "Crafted Reagents",
         items = {
             -- ID    = true,    --Item name
-            [173384] = true,    -- Crafter's Mark of the Chained Isle
-            [173383] = true,    -- Crafter's Mark III
-            [173382] = true,    -- Crafter's Mark II
-            [173381] = true,    -- Crafter's Mark I
-            [183942] = true,    -- Novice Mark II
-            [185960] = true,    -- Vestige of Origins
-        },
+            [173384] = true, -- Crafter's Mark of the Chained Isle
+            [173383] = true, -- Crafter's Mark III
+            [173382] = true, -- Crafter's Mark II
+            [173381] = true, -- Crafter's Mark I
+            [183942] = true, -- Novice Mark II
+            [185960] = true -- Vestige of Origins
+        }
     },
     ["Special_Reagents"] = {
         uiName = "Special reagents",
@@ -68,12 +68,12 @@ db.Filters = {
         title = "Zone Reagents",
         items = {
             -- ID    = true,    --Item name
-            [172331] = true,    -- Sinful Leather
-            [172332] = true,    -- Necrotic Leather
-            [172330] = true,    -- Unseelie Leather
-            [172333] = true,    -- Purified Leather
-        },
-    },
+            [172331] = true, -- Sinful Leather
+            [172332] = true, -- Necrotic Leather
+            [172330] = true, -- Unseelie Leather
+            [172333] = true -- Purified Leather
+        }
+    }
 }
 
 -- now that db is populated lets pass it on.
